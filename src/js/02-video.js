ce5180dc607d1daa16354e0265b0a9078ea21abe
nyotnoti08 <ynoalcala08@gmail.com>
2023-12-11 23:58:10 +0800

    Added Video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+
+const handleThrottle = function (data) {
+    const timeInSeconds = data.seconds;
+    console.log(timeInSeconds);
+    localStorage.setItem('videoplayer-current-time', timeInSeconds);
+}
+
+player.on('timeupdate', throttle(handleThrottle, 1000));
